8e8bc9aaf7f65ac4c76422cc40d228577735ba13
miguelxsm <mcarrascoguirao@gmail.com>
2025-12-16 11:54:33 -0500

    First commit

---
+++ main.py
@@ -0,0 +1,15 @@
+import os
+import time
+import json
+import requests
+import pathlib
+from infrastructur.destroy_infrastructure import destroy_all
+
+if __name__ == "__main__":
+
+    try:
+        ...
+
+    except KeyboardInterrupt:
+        print("CTRL + C Finishing Program...")
+        destroy
